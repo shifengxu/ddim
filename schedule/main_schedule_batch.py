0eb79cd8cacef95632a58093dfbb2281797497a1
CSLS34\shifeng <shifeng001@e.ntu.edu.sg>
2023-02-06 12:22:47 +0800

    schedule/main_schedule_batch.py: copy old comments from alpha_bar file into new alpha_bar file.

--- schedule/main_schedule_batch.py
+++ schedule/main_schedule_batch.py
@@ -68,7 +68,7 @@ def model_generate(alpha_bar, gpu_ids, device):
         model = DataParallel(model, device_ids=gpu_ids)
     return model
 
-def load_floats_from_file(f_path):
+def load_floats_from_file(f_path, c_arr):
     log_fn(f"load_floats_from_file() from file: {f_path}")
     with open(f_path, 'r') as f:
         lines = f.readlines()
@@ -82,6 +82,7 @@ def load_floats_from_file(f_path):
             continue
         if line.startswith('#'):
             cnt_comment += 1
+            c_arr.append(line)
             continue
         flt = float(line)
         f_arr.append(flt)
@@ -114,20 +115,23 @@ def main():
     log_fn(f"alpha_bar file cnt: {f_cnt}")
     for idx, f_path in enumerate(sorted(file_list)):
         log_fn(f"{idx:03d}/{f_cnt}: {f_path} ****************************")
-        alpha_bar = load_floats_from_file(f_path)
+        c_arr = [f" Old comments in file {f_path}"]  # comment array
+        alpha_bar = load_floats_from_file(f_path, c_arr)
+        c_arr = [c[1:].strip() for c in c_arr]  # remove prefix '#'
         alpha_bar = alpha_bar[1:]  # ignore the first one, as it is for timestep 0
+        _, idx_arr = vs(torch.tensor(alpha_bar, device=args.device), include_index=True)
+        s_arr = [f"{alpha_bar[i]:.8f} {idx_arr[i]:4d}" for i in range(len(alpha_bar))]
+        s_arr.insert(0, "Old alpha_bar and its timestep")
+        new_arr = c_arr + [''] + s_arr + [''] + m_arr
         f_name = os.path.basename(f_path)
         # if f_name != 'dpm_alphaBar_1-025-logSNR.txt': continue
-        train(args, vs, alpha_bar, m_arr, f_name)
+        train(args, vs, alpha_bar, new_arr, f_name)
     return 0
 
 def train(args, vs, alpha_bar, m_arr, f_name):
     model = model_generate(alpha_bar, args.gpu_ids, args.device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9)
     model.train()
-    _, idx_arr = vs(torch.tensor(alpha_bar, device=args.device), include_index=True)
-    s_arr = [f"{alpha_bar[i]:.8f} {idx_arr[i]:4d}" for i in range(len(alpha_bar))]
-    s_arr.insert(0, "Old alpha_bar and its timestep")
     start_time = time.time()
     loss_low = None
     loss_ori = None
@@ -152,9 +156,11 @@ def train(args, vs, alpha_bar, m_arr, f_name):
                    f" aa:{aacum[0]:.8f}~{aacum[-1]:.5f}. elp:{elp}, eta:{eta}")
             if loss_low is None or loss_low > loss.item():
                 loss_low = loss.item()
-                mm = m_arr + s_arr
-                mm.append(f"Epoch:{e_idx:06d}; loss:{loss:05.6f} = {loss_var:05.6f} + {loss_min:05.6f}")
-                mm.append(f"loss_var: {loss_ori:10.6f} => {loss_var:10.6f}")
+                mm = list(m_arr)
+                mm.append(f"Epoch        : {e_idx:06d}; loss:{loss:05.6f} = {loss_var:05.6f} + {loss_min:05.6f}")
+                mm.append(f"loss_var     : {loss_ori:10.6f} => {loss_var:10.6f}")
+                mm.append(f"model.learning_portion: {model.learning_portion}")
+                mm.append(f"model.out_channels    : {model.out_channels}")
                 detail_save(f_path, alpha, aacum, idx_arr, weight_arr, coef, numerator, sub_var, mm)
             # if
         # if
