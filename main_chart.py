09bf1f2496a130bafc9c62361d88574127a81e9f
CSLS34\shifeng <shifeng001@e.ntu.edu.sg>
2023-05-15 22:38:43 +0800

    main_chart.py: fid_ddim_cifar10.png, fid_ddim_celeba.png, fid_ddim_bedroom.png

--- main_chart.py
+++ main_chart.py
@@ -381,13 +381,115 @@ def mse_error_vs_alpha_bar():
     ax.legend(fontsize=25, loc='upper center')
 
     plt.show()
-    fig.savefig('./configs/chart/mse_error_vs_alpha_bar.png')
+    fig.savefig('./configs/chart/mse_error_vs_alpha_bar.png', bbox_inches='tight')
+    plt.close()
+
+def fid_compare_ddim_cifar10():
+    fid_log = [21.1, 12.7, 9.6, 8.0, 5.5, 4.7]    # logSNR
+    fid_lo2 = [15.2,  9.2, 7.4, 7.2, 4.7, 4.1]    # logSNR + VRG
+    fid_qua = [14.0,  8.9, 7.1, 6.1, 4.8, 4.3]    # quadratic
+    fid_qu2 = [11.9,  7.9, 6.2, 5.7, 4.3, 3.8]    # quadratic + VRG
+    fid_uni = [16.8, 10.7, 8.3, 7.1, 4.8, 4.0]    # uniform
+    fid_un2 = [11.1,  7.7, 6.1, 5.8, 4.4, 3.8]    # uniform + VRG
+    steps = ['10', '15', '20', '25', '50', '100']
+    fig = plt.figure(figsize=(16, 7))
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+    # plt.xlim((0, 1.02))
+    # plt.ylim((0, 1800))
+    axs = [ax1, ax2, ax3]
+    fid_arr = [[fid_log, fid_lo2], [fid_qua, fid_qu2], [fid_uni, fid_un2]]
+    lbl_arr = [[s, f"{s}+VRG"] for s in ['logSNR', 'quadratic', 'uniform']]
+    ylb_arr = [r"FID      ", '', '']    # y label on the left
+    xlb_arr = ['', r"step count", '']   # x label in the middle
+    for ax, fids, lbs, ylb, xlb in zip(axs, fid_arr, lbl_arr, ylb_arr, xlb_arr):
+        ax.set_ylabel(ylb, fontsize=25, rotation=0)  # make it horizontal
+        ax.set_xlabel(xlb, fontsize=25)
+        ax.tick_params('both', labelsize=20)
+        ax.plot(steps, fids[0], linestyle='-', color='c', marker='o', label=lbs[0])
+        ax.plot(steps, fids[1], linestyle='-', color='r', marker='s', label=lbs[1])
+        ax.legend(fontsize=18, loc='upper right')
+    # for
+    plt.show()
+    f_path = './configs/chart/fid_compare_ddim.png'
+    fig.savefig(f_path, bbox_inches='tight')
+    print(f"file saved: {f_path}")
+    plt.close()
+
+def fid_compare_ddim_bedroom():
+    fid_log = [46.2, 29.5, 23.1]    # logSNR
+    fid_lo2 = [43.1, 27.0, 20.9]    # logSNR + VRG
+    fid_qua = [35.1, 23.2, 19.0]    # quadratic
+    fid_qu2 = [34.1, 22.7, 18.8]    # quadratic + VRG
+    fid_uni = [20.1, 13.4, 11.0]    # uniform
+    fid_un2 = [18.6, 12.2, 10.7]    # uniform + VRG
+    steps = ['10', '15', '20']
+    fig = plt.figure(figsize=(16, 7))
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+    # plt.xlim((0, 1.02))
+    # plt.ylim((0, 1800))
+    axs = [ax1, ax2, ax3]
+    fid_arr = [[fid_log, fid_lo2], [fid_qua, fid_qu2], [fid_uni, fid_un2]]
+    lbl_arr = [[s, f"{s}+VRG"] for s in ['logSNR', 'quadratic', 'uniform']]
+    ylb_arr = [r"FID      ", '', '']    # y label on the left
+    xlb_arr = ['', r"step count", '']   # x label in the middle
+    for ax, fids, lbs, ylb, xlb in zip(axs, fid_arr, lbl_arr, ylb_arr, xlb_arr):
+        ax.set_ylabel(ylb, fontsize=25, rotation=0)  # make it horizontal
+        ax.set_xlabel(xlb, fontsize=25)
+        ax.tick_params('both', labelsize=20)
+        ax.plot(steps, fids[0], linestyle='-', color='c', marker='o', label=lbs[0])
+        ax.plot(steps, fids[1], linestyle='-', color='r', marker='s', label=lbs[1])
+        ax.legend(fontsize=18, loc='upper right')
+    # for
+    plt.show()
+    f_path = './configs/chart/fid_ddim_bedroom.png'
+    fig.savefig(f_path, bbox_inches='tight')
+    print(f"file saved: {f_path}")
+    plt.close()
+
+def fid_compare_ddim_celeba():
+    fid_log = [18.1, 12.1, 9.7]     # logSNR
+    fid_lo2 = [15.9, 10.8, 8.7]     # logSNR + VRG
+    fid_qua = [28.9, 19.4, 14.5]    # quadratic
+    fid_qu2 = [27.0, 16.2, 11.9]    # quadratic + VRG
+    fid_uni = [15.4, 9.6, 7.1]      # uniform
+    fid_un2 = [14.7, 9.2, 7.0]      # uniform + VRG
+    steps = ['10', '15', '20']
+    fig = plt.figure(figsize=(16, 7))
+    ax1 = fig.add_subplot(1, 3, 1)
+    ax2 = fig.add_subplot(1, 3, 2)
+    ax3 = fig.add_subplot(1, 3, 3)
+    # plt.xlim((0, 1.02))
+    # plt.ylim((0, 1800))
+    axs = [ax1, ax2, ax3]
+    fid_arr = [[fid_log, fid_lo2], [fid_qua, fid_qu2], [fid_uni, fid_un2]]
+    lbl_arr = [[s, f"{s}+VRG"] for s in ['logSNR', 'quadratic', 'uniform']]
+    ylb_arr = [r"FID      ", '', '']    # y label on the left
+    xlb_arr = ['', r"step count", '']   # x label in the middle
+    for ax, fids, lbs, ylb, xlb in zip(axs, fid_arr, lbl_arr, ylb_arr, xlb_arr):
+        ax.set_ylabel(ylb, fontsize=25, rotation=0)  # make it horizontal
+        ax.set_xlabel(xlb, fontsize=25)
+        ax.tick_params('both', labelsize=20)
+        ax.plot(steps, fids[0], linestyle='-', color='c', marker='o', label=lbs[0])
+        ax.plot(steps, fids[1], linestyle='-', color='r', marker='s', label=lbs[1])
+        ax.legend(fontsize=18, loc='upper right')
+    # for
+    plt.show()
+    f_path = './configs/chart/fid_ddim_celeba.png'
+    fig.savefig(f_path, bbox_inches='tight')
+    print(f"file saved: {f_path}")
     plt.close()
 
 def main():
     # fig_trajectory_ori_vs_new()
     # merge_image()
-    mse_error_vs_alpha_bar()
+    # mse_error_vs_alpha_bar()
+    # fid_compare_ddim_cifar10()
+    # fid_compare_ddim_bedroom()
+    fid_compare_ddim_celeba()
 
 if __name__ == '__main__':
     main()
